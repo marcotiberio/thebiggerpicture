d7a493d4bdb1dd667fd64306b197d594c84ca6dc
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2019-09-10 09:40:36 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -9,3 +9,23 @@ function delayShow() {
   var secs = 1000;
   setTimeout('jQuery("body").css("visibility","visible");', secs);
 }
+
+// blink "on" state
+function show()
+{
+  if (document.getElementById)
+  document.getElementById("opacity").style.visibility = "hidden";
+}
+// blink "off" state
+function hide()
+{
+  if (document.getElementById)
+  document.getElementById("opacity").style.visibility = "visible";
+}
+// toggle "on" and "off" states every 450 ms to achieve a blink effect
+// end after 4500 ms (less than five seconds)
+for(var i=900; i < 7500; i=i+900)
+{
+  setTimeout("show()",i+450);
+  setTimeout("hide()",i);
+}
