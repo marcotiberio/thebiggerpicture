c4c93a32999220e3bfbbd0dc7a678b8571ff9779
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2019-09-11 15:35:55 +0200

    Update image-fading.js

--- image-fading.js
+++ image-fading.js
@@ -1,19 +1,19 @@
 $next = 1;			// fixed, please do not modfy;
 $current = 0;		// fixed, please do not modfy;
-$interval = 5000;	// You can set single picture show time;
-$fadeTime = 1000;	// You can set fadeing-transition time;
+$interval = 4000;	// You can set single picture show time;
+$fadeTime = 2000;	// You can set fadeing-transition time;
 
 $(document).ready(function(){
 	//NOTE : Div Wrapper should with css: relative;
 	//NOTE : img should with css: absolute;
 	//NOTE : img Width & Height can change by you;
-	$('#oapcity').css('position','relative');
+	$('#opacity').css('position','absolute');
 
 	nextFadeIn();
 });
 
 function nextFadeIn(){
-	//make image fade in and fade out at one time, without splash vsual;
-	$('#opacity').eq($current).delay($interval).fadeOut($fadeTime)
-	.end().eq($next).delay($interval).hide().fadeIn($fadeTime, nextFadeIn);
+	//make image fade in and fade out at one time, without splash visual;
+	$('#opacity').eq($next).delay($interval).fadeOut($fadeTime)
+	.end().eq($current).delay($interval).hide().fadeIn($fadeTime).fadeOut($fadeTime).end();
 };
