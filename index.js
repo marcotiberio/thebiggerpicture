cbba2f879351c9b2b595b6035f228e2eaa35ebd7
Marco Tiberio <marco@defrostudio.eu>
2019-10-10 16:23:53 +0200

    updates

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ jQuery(document).ready(function() {
   /* Show the HTML page only after the js and css are completely loaded */
   delayShow();
 });
+
+
+// STOP PULSING ANIMATION
+document.getElementById('blink1').addEventListener(
+    'webkitAnimationEnd',
+    function(){
+        document.getElementById('blink1').style.display = 'none';
+        document.getElementById('blink2').style.display = 'none';
+        document.getElementById('blink3').style.display = 'none';
+    },
+    false
+);
